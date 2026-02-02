ed7090d51dc18a5b4a01bc52d3148be360bec488
snowdingo <snowdingo.tech@gmail.com>
2026-02-02 23:14:21 +0900

    no you can terminate the app

--- source/app.js
+++ source/app.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
-import {Text,Box, Newline} from 'ink';
+import {Text,Box, Newline, useApp} from 'ink';
 import BigText from 'ink-big-text';
 import Spinner from 'ink-spinner';
 import TextInput from 'ink-text-input';
-import { Alert } from '@inkjs/ui';
+import { Alert, MultiSelect, Select } from '@inkjs/ui';
+
+
 
 export default function App() {
 
-	const [todo, setTodo] = useState("")
-	const [previous, setPrevious] = useState("")
-	const [submitted, setSubmitted] = useState(false)
+	const {exit} = useApp();
+
+	const [option, setOption] = useState(100)
 	return (
 		<Box flexDirection='column'>
 			<Text color={"cyan"}><BigText colors={"red"} text='STEAM-CLI'></BigText></Text>
@@ -17,6 +19,36 @@ export default function App() {
 		<Newline />
 
 		<Text color={'yellow'}>Please type in /help to see all the available commands</Text>
+		<Box>
+			<Select
+				options={[
+					{
+						label:"Search for a game",
+						value:1,
+					},
+					{
+						label:"Search for a user",
+						value:2,
+					},
+					{
+						label:"exit",
+						value:99,
+					}
+				]}
+
+				onChange={(e) => {
+					setOption(e)
+					if(e ==99){
+						exit();
+					}
+				}}
+
+				
+			>
+
+				
+			</Select>
+		</Box>
 		{/* <Box>
 			{submitted?<Alert variant='success'>Created {previous}</Alert>:<></>}
 		</Box> */}
