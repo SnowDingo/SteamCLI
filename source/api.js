f486f623d8e190759454de239fb1bdc1cd91f29b
snowdingo <snowdingo.tech@gmail.com>
2026-02-10 12:52:24 +0900

    did prettier code formatting and now it sets the loading screen

---
+++ source/api.js
@@ -0,0 +1,25 @@
+
+// function to turn vanityURL into apikey meaning pretty much it is a function that is supposed to be called when we need to
+// convert username into an unique Steamid.
+async function fetchSteamID(apikey, vanityURL){
+    const url = `https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${apiKey}&vanityurl=${vanity}`;
+    const response = await fetch(url);
+    // turn the fetched result into json format cause it will be much easier to manipulate the data
+    const resultingjson = await response.json();
+    if(resultingjson.response.success == 42){
+        // In the app side if the steam ID is 0 it will throw error and prompt the user to try again.
+        return 0;
+    }else{
+        return resultingjson.response.steamid;
+    }
+}
+
+
+// function to fetch user's basic data
+
+async function getUserData(apikey, userid){
+    const url = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apikey}&steamids=${userid}`;
+    const response = await fetch(url);
+    const resultjson = response.json();
+    return resultjson.players[0]
+}
--- source/app.js
+++ source/app.js
@@ -1,114 +1,158 @@
-import React, { useState } from 'react';
-import {Text,Box, Newline, useApp, useInput} from 'ink';
+import React, {useEffect, useState} from 'react';
+import {Text, Box, Newline, useApp, useInput} from 'ink';
 import BigText from 'ink-big-text';
 import Spinner from 'ink-spinner';
 import TextInput from 'ink-text-input';
-import { Alert, MultiSelect, Select } from '@inkjs/ui';
+import {Alert, MultiSelect, Select} from '@inkjs/ui';
+import Link from 'ink-link';
 
+// So here are some of the basic structure for this app
+// There is an useState variable called option that tracks the state
+/*
+	101-> Input the API Key
+	100->Menu
+	1->Enter the user name
+	105->Calling APIs
+	200->Display results
+
+*/
 
 export default function App() {
-
 	const {exit} = useApp();
 
-	const [option, setOption] = useState(101)
+	const [option, setOption] = useState(101);
 
-	const [api, setAPI] = useState("")
-	
+	const [api, setAPI] = useState('');
+
 	// A textfield related stuff yay!
-	const [inputval, setInputValue] = useState("")
+	const [inputval, setInputValue] = useState('');
 
+	const [loading, setLoading] = useState(false);
+
+	// useEffect
+	useEffect(() => {
+		// if the user hasn't done all the required steps just go back
+		if (option != 105) {
+			return;
+		}
+		setLoading(true);
+	}, [option]);
 
 	const UserInput = () => {
 		useInput((input, key) => {
-			if(input == 'x'){
-				if(option==2){
-					setOption(100)
-					setInputValue("")
-				}else if(option==1){
-					setOption(100)
-					setInputValue("")
+			if (input == 'x') {
+				if (option == 2) {
+					setOption(100);
+					setInputValue('');
+				} else if (option == 1) {
+					setOption(100);
+					setInputValue('');
 				}
 			}
-			if(key.return && option == 101){
-				setOption(100)
+			if (key.return) {
+				if (option == 101) {
+					setOption(100);
+				} else if (option == 2) {
+					setOption(105);
+				}
 			}
-		})
-	}
+		});
+	};
 
-	return (
-		<Box flexDirection='column'>
-			<UserInput />
-			<Text color={"cyan"}><BigText colors={"red"} text='STEAM-CLI'></BigText></Text>
-			<Text>A cli app to use steam API</Text>
-		<Newline />
-
-		<Text color={'yellow'}>Please type in /help to see all the available commands</Text>
-		<Newline />
-		{(option == 101)?<>
-			<Text color={"white"}>Please enter the Steam API key <Text color={"red"}>Note: You have to retype this in future uses</Text> </Text>
-					<Box borderStyle={"round"} borderColor={"white"}>
-					<TextInput value={api}  onChange={(e) => {
-						setAPI(e)
-					}} placeholder='Enter the Steam API key'></TextInput>
-					</Box>
-		</>:<>
-			{(option ==100)?<>
-			<Text color={"redBright"}> Your steam API key: {api} </Text>
-				<Newline/>
+	return loading ? (
+		<Box></Box>
+	) : (
+		<>
+			<Box flexDirection="column">
+				<UserInput />
+				<Text color={'cyan'}>
+					<BigText colors={'red'} text="STEAM-CLI"></BigText>
+				</Text>
+				<Text>A cli app to use steam API</Text>
 				<Newline />
-			<Box>
-			<Select
-				options={[
-					{
-						label:"Search for a game",
-						value:1,
-					},
-					{
-						label:"Search for a user",
-						value:2,
-					},
-					{
-						label:"exit",
-						value:99,
-					}
-				]}
 
-				onChange={(e) => {
-					setOption(e)
-					if(e ==99){
-						exit();
-					}else if(e=1){
-						
-					}
-				}}
-
-				
-			>
-
-				
-			</Select>
-		</Box>
-		</>:<>
-				{option == 1?<>
-					<Text>Please enter the name of the game </Text>
-					<Box borderStyle={"round"} borderColor={"white"}>
-					<TextInput value={inputval}  onChange={(e) => {
-						setInputValue(e)
-					}} placeholder='Enter the Steam game title'></TextInput>
-					</Box>
-				</>:<>
-				{option ==2?<>
-
-					<Text color={"white"}>Please enter the user name. <Text color={"red"}>Note: This has to be public profile</Text> </Text>
-					<Box borderStyle={"round"} borderColor={"white"}>
-					<TextInput value={inputval}  onChange={(e) => {
-						setInputValue(e)
-					}} placeholder='Enter the Steam username'></TextInput>
-					</Box>
-				</>:<></>}
-				</>}
-		</> }
-		
-		</>}
-	</Box>
-	)}
+				<Text color={'yellow'}>
+					Please type in /help to see all the available commands
+				</Text>
+				<Newline />
+				{option == 101 ? (
+					<>
+						<Text color={'white'}>
+							Please enter the Steam API key{' '}
+							<Text color={'red'}>
+								Note: You have to retype this in future uses.
+								<br /> You can get your own Steam API Keys{' '}
+								<Link url="https://steamcommunity.com/dev/apikey">
+									here
+								</Link>{' '}
+							</Text>{' '}
+						</Text>
+						<Box borderStyle={'round'} borderColor={'white'}>
+							<TextInput
+								value={api}
+								onChange={e => {
+									setAPI(e);
+								}}
+								placeholder="Enter the Steam API key"
+							></TextInput>
+						</Box>
+					</>
+				) : (
+					<>
+						{option == 100 ? (
+							<>
+								<Box>
+									<Select
+										options={[
+											{
+												label: 'Search for a user',
+												value: 1,
+											},
+											{
+												label: 'exit',
+												value: 99,
+											},
+										]}
+										onChange={e => {
+											setOption(e);
+											if (e == 99) {
+												exit();
+											} else if (e == 1) {
+											}
+										}}
+									></Select>
+								</Box>
+							</>
+						) : (
+							<>
+								<>
+									{option == 1 ? (
+										<>
+											<Text color={'white'}>
+												Please enter the user name.{' '}
+												<Text color={'red'}>
+													Note: This has to be public profile
+												</Text>{' '}
+											</Text>
+											<Box borderStyle={'round'} borderColor={'white'}>
+												<TextInput
+													value={inputval}
+													onChange={e => {
+														setInputValue(e);
+													}}
+													placeholder="Enter the Steam username"
+												></TextInput>
+											</Box>
+										</>
+									) : (
+										<></>
+									)}
+								</>
+							</>
+						)}
+					</>
+				)}
+			</Box>
+		</>
+	);
+}
